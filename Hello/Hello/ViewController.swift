2804a4038556f7aae5c605dabd50793ebcc0dfab
student <student@dc-98.3500.dhcp.asu.edu>
2019-08-27 10:54:35 -0700

    assignment 1

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by student on 8/27/19.
+//  Copyright © 2019 ASU. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
