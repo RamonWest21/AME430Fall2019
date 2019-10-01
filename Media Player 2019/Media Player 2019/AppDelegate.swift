ec3b5a88217425ef7f95b05c260e160a86472105
student <student@dc-98.3500.dhcp.asu.edu>
2019-10-01 14:52:06 -0700

    media player 2019

---
+++ Media Player 2019/Media Player 2019/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Media Player 2019
+//
+//  Created by student on 10/1/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Media Player 2019/Media Player 2019/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Media Player 2019
+//
+//  Created by student on 10/1/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import Cocoa
+import AVKit
+
+class ViewController: NSViewController {
+    
+    
+    @IBOutlet weak var playerView: AVPlayerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+
+}
+
